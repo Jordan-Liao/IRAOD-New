4ec6c3a0f1b0279c2e9193570c33273bd262edab
Jordan-Liao <173348718@qq.com>
2026-06-07 21:46:42 +0800

    Update

---
+++ configs/unbiased_teacher/sfod/unbiased_teacher_oriented_rcnn_selftraining_cga_rsar_orthonet.py
@@ -0,0 +1,19 @@
+_base_ = './unbiased_teacher_oriented_rcnn_selftraining_cga_rsar1.py'
+
+custom_imports = dict(
+    imports=['sfod', 'mmdet_extension'],
+    allow_failed_imports=False)
+
+model = dict(
+    backbone=dict(
+        _delete_=True,
+        type='OrthoNet',
+        depth=50,
+        reduction=16,
+        in_channels=3,
+        num_stages=4,
+        out_indices=(0, 1, 2, 3),
+        frozen_stages=1,
+        norm_eval=True,
+        style='pytorch',
+        init_cfg=None))
